6a2f60e607aa819287589e3090c11215b3e7929b
Donnz9 <donihermawwan@gmail.com>
2025-02-27 08:49:24 +0700

    first upload

---
+++ android/app/src/main/kotlin/com/example/bkpm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bkpm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,26 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:bkpm/main.dart';
+
+void main() {
+  // int num1 = 10;
+  // double num2 = 10.50;
+  // print(num1);
+  // print(num2);
+
+  // print(num.parse('12'));
+  // print(num.parse('10.91'));
+
+  // print(num.parse('12A'));
+  // print(num.parse('AAA'));
+
+  
+}
